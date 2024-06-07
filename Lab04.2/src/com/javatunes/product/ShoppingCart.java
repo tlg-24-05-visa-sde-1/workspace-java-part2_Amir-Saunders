a442e4f34b5cee5454884cb4899f9d0f94de70ce
Amir Saunders <AmirS@tlgcohort.com>
2024-06-07 16:23:43 -0500

    Lab 4.2 complete

--- Lab04.2/src/com/javatunes/product/ShoppingCart.java
+++ Lab04.2/src/com/javatunes/product/ShoppingCart.java
@@ -12,9 +12,27 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
-public class ShoppingCart<T> {
+public class ShoppingCart<T extends Product> {
+//    above this means that anything that extends product can go in this list. T is a placeholder
     // storage for the cart's contents
     private Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
+
+
+    public double total(){
+        double result = 0.0;
+        for(T item : items){
+            result += item.getPrice();
+        }
+        return result;
+    }
+    public int size(){
+        return items.size();
+    }
+
+    public  Collection <T> allItems(){
+        return Collections.unmodifiableCollection(items);
+    };
+
 
     public void addItem(T item) {
         items.add(item);
